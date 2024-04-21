696a802c1c2b41850b4bd70d7faac034a7e9061a
Andrew Kang <yanjunk@students.cs.ubc.ca>
2024-04-21 08:43:57 -0700

    XIRR - use new csv format

--- xirr/main.py
+++ xirr/main.py
@@ -1,11 +1,12 @@
+import datetime
+import pathlib
+
+import dateutil
 import numpy as np
 import pandas as pd
-import datetime
-import models
-import pathlib
-import dateutil
+import pyxirr
 
-debug = lambda x: x
+import models
 
 
 def expect(cond, message):
@@ -21,81 +22,155 @@ def get_monthend_date(date):
 
 
 if __name__ == "__main__":
-    # todo: make this based off a single date
-    config = models.Config(
-        {
-            "QD": lambda d: d.replace(month=(((d.month - 1) // 3) * 3 + 1), day=1)
+    custom_intervals = pd.read_csv(
+        pathlib.Path(__file__).parent / "settings" / "intervals.csv",
+        parse_dates=["StartDate", "EndDate"],
+    )
+    config = {
+        "QD": lambda d: (
+            d[1].replace(month=(((d[1].month - 1) // 3) * 3 + 1), day=1)
             + dateutil.relativedelta.relativedelta(days=-1),
-            "YD": lambda d: d.replace(month=1, day=1)
+            d[1],
+        ),
+        "YD": lambda d: (
+            d[1].replace(month=1, day=1)
             + dateutil.relativedelta.relativedelta(days=-1),
-            "XIRR-1": lambda d: d + dateutil.relativedelta.relativedelta(years=-1),
-            "XIRR-3": lambda d: d + dateutil.relativedelta.relativedelta(years=-3),
-            "XIRR-5": lambda d: d + dateutil.relativedelta.relativedelta(years=-5),
-            "XIRR-10": lambda d: d + dateutil.relativedelta.relativedelta(years=-10),
-            "XIRR-INC": lambda d: d + dateutil.relativedelta.relativedelta(years=-1000),
-        }
+            d[1],
+        ),
+        "XIRR-1": lambda d: (
+            d[1] + dateutil.relativedelta.relativedelta(years=-1),
+            d[1],
+        ),
+        "XIRR-3": lambda d: (
+            d[1] + dateutil.relativedelta.relativedelta(years=-3),
+            d[1],
+        ),
+        "XIRR-5": lambda d: (
+            d[1] + dateutil.relativedelta.relativedelta(years=-5),
+            d[1],
+        ),
+        "XIRR-10": lambda d: (
+            d[1] + dateutil.relativedelta.relativedelta(years=-10),
+            d[1],
+        ),
+        "XIRR-INC": lambda d: d,
+        **{
+            row.Name: lambda d: (row.StartDate, row.EndDate)
+            for row in custom_intervals.itertuples()
+        },
+    }
+
+    cashflows = pd.read_csv(
+        pathlib.Path(__file__).parent / "data" / "CashFlows.csv",
+        parse_dates=["TRADDATE"],
+    )
+    month_end = pd.read_csv(
+        pathlib.Path(__file__).parent / "data" / "MonthEndMVs.csv",
+        parse_dates=["as_of_date"],
+    )
+    starting = pd.read_csv(
+        pathlib.Path(__file__).parent / "data" / "StartingMVs.csv",
+        parse_dates=["STARTMV_DATE"],
     )
 
-    data = pd.read_excel(
-        (pathlib.Path(__file__).parent / "test.xlsm").resolve(), sheet_name=None
+    counts = starting["PortCode"].value_counts()
+
+    expect(
+        starting["PortCode"].is_unique,
+        f"Expected 'PortCode' in startingMVs to be unique. However, port codes {', '.join([str(a) for a in counts[counts > 1].index.values])} are repeated.",
     )
 
     ret = {}
 
-    for df in data.values():
-        for x, y in zip(*np.where(df.values == "test_token")):
-            debug(f"Found token at coordinates {(x, y)} on sheet {df}, parsing.")
-            expect(
-                df.iat[x + 1, y] == "name:",
-                f'Expected "name:" at cell {x+ 1, y}, found {df.iat[x + 1, y]}',
+    for account in starting["PortCode"]:
+        # add + or - depending on transaction type
+        aflows = (
+            cashflows[cashflows["PortCode"] == account]
+            .apply(
+                lambda x: (
+                    x.iloc[0],
+                    x.iloc[1],
+                    x.iloc[2],
+                    x.iloc[3] * (-1 if x.iloc[2] == "WITHDRAWAL" else 1),
+                ),
+                axis=1,
+                result_type="broadcast",
             )
-            expect(
-                isinstance(df.iat[x + 1, y + 1], str),
-                f"Expected string at cell {x + 1, y+1}, found {df.iat[x + 1, y+1]}",
-            )
-            name = df.iat[x + 1, y + 1]
-            expect(
-                name not in ret,
-                f"Found name {name} already in ret. Since this will override the previous value, have each name be unique.",
-            )
-            expect(
-                df.iat[x + 2, y] == "amounts:",
-                f'Expected "amounts:" at cell {x, y+1}, found {df.iat[x + 2, y]}',
-            )
-            i = 3
-            entry_list = models.EntryList()
-            dates = []
-            while df.iat[x + i, y] != "end":
-                val = df.iat[x + i, y]
-                date = df.iat[x + i, y + 1]
-                parsed_date = None
-                expect(
-                    isinstance(val, (int, float)),
-                    f"Expected cell {x + i, y} to contain a number, found {df.iat[x + i, y]}",
-                )
-                try:
-                    if isinstance(date, datetime.datetime):
-                        parsed_date = date.date() 
-                    else:
-                        parsed_date = datetime.strptime(date, "%d-%m-%Y").date()
-                except:
-                    expect(
-                        False,
-                        f"Expected cell {x + i, y+ 1} to contain a date, found {df.iat[x + i, y + 1]}",
-                    )
-                i += 1
-                if parsed_date.month not in [3, 6, 9, 12]:
-                    continue
-                entry_list.append(models.Entry(val, get_monthend_date(parsed_date)))
+            .rename(columns={"TRADDATE": "Date", "NETAMTC": "Value"})
+        )
 
-            ret[name] = config.compute_XIRR_table(entry_list)
+        aend = (
+            month_end[month_end["PortCode"] == account]
+            .rename(columns={"MarketValue": "Value", "as_of_date": "Date"})
+            .apply(
+                lambda x: (x.iloc[0], x.iloc[1], get_monthend_date(x.iloc[2])),
+                axis=1,
+                result_type="broadcast",
+            )
+        )
+        aend = aend.loc[pd.to_datetime(aend["Date"]).dt.month % 3 == 0]
+        final_date = aend["Date"].max()
+        astart = starting[starting["PortCode"] == account].rename(
+            columns={"OPENING_MKTVAL": "Value", "STARTMV_DATE": "Date"}
+        )
+
+        aflows = aflows[["Date", "Value"]]
+        aflows["Type"] = 1
+        aend = aend[["Date", "Value"]]
+        aend["Type"] = 2
+        astart = astart[["Date", "Value"]]
+        aend["Type"] = 0
+
+        vals = (
+            pd.concat(
+                [
+                    astart,
+                    aend[aend["Date"] != aend["Date"].max()],
+                    aend.apply(
+                        lambda x: (x.iloc[0], -x.iloc[1], x.iloc[2]),
+                        axis=1,
+                        result_type="broadcast",
+                    ),
+                    aflows,
+                ]
+            )
+            .sort_values(["Date", "Type", "Value"], axis="index")
+            .drop(["Type"], axis="columns")
+            .reset_index(drop=True)
+        )
+
+        #todo: redo this
+        # there arel ikely small bugs with cashflows on same day
+
+        acc_dict = {}
+        for name, get_bounds in config.items():
+            start_date, end_date = get_bounds((astart["Date"].min(), final_date))
+            if astart["Date"].min() <= start_date and end_date <= final_date:
+                first_index = vals[start_date <= vals["Date"]].index[0]
+                last_index = vals[vals["Date"] <= end_date].index[-1]
+                acc_dict[name] = (
+                    pyxirr.xirr(
+                        vals[
+                            (start_date <= vals["Date"])
+                            & (vals["Date"] <= end_date)
+                            & ((vals.index != first_index) | (vals["Value"] >= 0))
+                            & ((vals.index != last_index) | (vals["Value"] <= 0))
+                        ]
+                    )
+                    + 1
+                ) ** (min(1, (end_date - start_date).days / 365)) - 1
+            else: 
+                acc_dict[name] = None
+
+        ret[account] = acc_dict
 
     ret_df = (
         pd.DataFrame(
-            data={k: list(v.values()) for k, v in ret.items()}, index=config.get_keys()
+            data={k: list(v.values()) for k, v in ret.items()}, 
+            index=config.keys()
         )
     ).transpose()
+    # todo: fix formatting
     ret_df_percent = (ret_df * 100).round(2).map(lambda x: f"{x}%")
     ret_df_percent.to_csv("output.csv")
     print("Output saved to output.csv")
-
