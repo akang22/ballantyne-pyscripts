5b031384b44122828d676005989379a7fc258563
Andrew Kang <yanjunk@students.cs.ubc.ca>
2024-05-23 22:01:34 -0700

    asdf

--- run.py
+++ run.py
@@ -10,12 +10,12 @@ if __name__ == '__main__':
     iswin = platform.system() == 'Windows'
     f = open("log.txt", "a")
     def run(command):
-        if !iswin:
+        if not iswin:
             command = shlex.split(command)
         subprocess.run(command, stdout=subprocess.DEVNULL)
 
     def run_async(command):
-        if !iswin:
+        if not iswin:
             command = shlex.split(command)
         df = subprocess.Popen(command, stderr=f, stdout=subprocess.DEVNULL)
     print("Updating codebase...")
