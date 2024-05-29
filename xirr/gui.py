2ecca556d0e20435bb09e2a6813adadbd4fe993e
Andrew Kang <yanjunk@students.cs.ubc.ca>
2024-05-29 07:26:45 -0700

    XIRR - add downloadability

--- xirr/gui.py
+++ xirr/gui.py
@@ -1,20 +1,21 @@
 import streamlit as st
+import pandas as pd
 from main import main_func
 
 st.title("XIRR Applet")
-tabs = st.tabs(["Database Input (wip)", "CSV input", "Walkthrough (wip)"])
+tabs = st.tabs(["CSV input", "Database Input (wip)",  "Walkthrough (wip)"])
 
-cashflow = tabs[1].file_uploader("Cashflows", type=["csv"])
-monthend = tabs[1].file_uploader("Month End Values", type=["csv"])
-starting = tabs[1].file_uploader("Account Starting Values", type=["csv"])
+cashflow = tabs[0].file_uploader("Cashflows", type=["csv"])
+monthend = tabs[0].file_uploader("Month End Values", type=["csv"])
+starting = tabs[0].file_uploader("Account Starting Values", type=["csv"])
 
 for i, tab in enumerate(tabs[:-1]):
     submitted = tab.button("Submit", key=f"submit${i}")
     if submitted:
         res_data_frame, interim = main_func(cashflow, monthend, starting, return_interim=True)
         tab.header("Final Result")
-        tab.table(res_data_frame)
+        tab.dataframe(res_data_frame, use_container_width=True)
         tab.header("Interim Results")
         for account, df in interim.items():
             tab.subheader(f"{account}")
-            tab.table(df)
+            tab.dataframe(pd.DataFrame(df), use_container_width=True)
